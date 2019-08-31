c2e9d3fda86b8d7032ad937608fb1732065f60c1
Kunal Singh <kunz.kunal@hotmail.com>
2019-08-31 23:38:52 +1200

    Create iomainp.cpp

---
+++ iomainp.cpp
@@ -0,0 +1,18 @@
+/*including libraries*/
+#include <iostream> //I/O header file for C++
+#include <stdlib.h> //Defines four variable types, several macros, and various functions for performing general functions.
+#include <iomanip> //setting precision points
+
+using namespace std; //The built in C++ library routines are kept in the standard namespace.like cin cout.. etc
+
+	int main ()
+	{
+		double month1 = 1061.2121213123213294;
+		cout<<setprecision(5);
+		cout<<month1<<endl;
+	
+
+	  return 0;
+	}
+	
+
